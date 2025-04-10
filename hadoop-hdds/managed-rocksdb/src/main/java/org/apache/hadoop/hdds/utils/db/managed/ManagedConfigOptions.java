6e4147ef24acf19d3978adb7a6d3b6adca3ac93b
Siyao Meng <50227127+smengcl@users.noreply.github.com>
2025-04-09 17:36:56 -0700

    checkstyle

--- hadoop-hdds/managed-rocksdb/src/main/java/org/apache/hadoop/hdds/utils/db/managed/ManagedConfigOptions.java
+++ hadoop-hdds/managed-rocksdb/src/main/java/org/apache/hadoop/hdds/utils/db/managed/ManagedConfigOptions.java
@@ -1,11 +1,10 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -14,15 +13,15 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
+
 package org.apache.hadoop.hdds.utils.db.managed;
+
+import static org.apache.hadoop.hdds.utils.db.managed.ManagedRocksObjectUtils.track;
 
 import org.apache.ratis.util.UncheckedAutoCloseable;
 import org.rocksdb.ConfigOptions;
 import org.rocksdb.Env;
-
-import static org.apache.hadoop.hdds.utils.db.managed.ManagedRocksObjectUtils.track;
 
 /**
  * Managed ConfigOptions.
@@ -35,11 +34,11 @@ public class ManagedConfigOptions extends ConfigOptions {
   }
 
   public ManagedConfigOptions setIgnoreUnknownOptions(final boolean ignore) {
-    return (ManagedConfigOptions)super.setIgnoreUnknownOptions(ignore);
+    return (ManagedConfigOptions) super.setIgnoreUnknownOptions(ignore);
   }
 
   public ManagedConfigOptions setEnv(final Env env) {
-    return (ManagedConfigOptions)super.setEnv(env);
+    return (ManagedConfigOptions) super.setEnv(env);
   }
 
   @Override
